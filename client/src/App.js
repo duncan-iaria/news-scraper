bc045b14cf069169c32c588d240b7dee02bdfccf
Snoodleking <duncaniaria@gmail.com>
2017-09-10 21:54:33 -0400

    set up proxy for client - made test data show up from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends Component 
+{
+	  
+	state = { users: [] }
+
+	componentDidMount() 
+	{
+		fetch( '/users' ).then( res => res.json() ).then( users => this.setState( { users } ) );
+	}
+
+	render() 
+	{
+		return (
+			<div className="App">
+			<h1>Users</h1>
+			{ this.state.users.map( user => <div key={ user.id }> { user.name }</div> ) }
+			</div>
+		);
+	}
 }
 
 export default App;
