42956ca14ca09edffe62998d12003d4ed0281fe8
Snoodleking <duncaniaria@gmail.com>
2017-09-14 09:46:21 -0400

    added comment explaination for model index

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,11 @@
 //=========================
 // ALL MODELS
 //=========================
+//So this is totally overkill for this particular app
+//however I wanted to try and get similar functionality
+//to how sequelize gives you access to ALL models
+//in theory you can add more and more keys and models
+//and just call db.ModelYouWant after requiring the models folder
 const db =
 {
     Article: require( './Article' ),
